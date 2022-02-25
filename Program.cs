f8ea176bc23c5a39c3487f58ba17c6c1e397ef82
JOY <joybosu@gmail.com>
2022-02-25 23:09:36 +0530

    Add welcome to AddressBook

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NoteBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program ");
+            CreateContacts name=new CreateContacts();
+            name.PersonDetails();
+        }
+    }
+}
+
