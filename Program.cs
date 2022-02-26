583d7f8ad9b2a7edf452681d4396813866edbf0b
JOY <joybosu@gmail.com>
2022-02-27 00:55:08 +0530

    Add another person to book

--- Program.cs
+++ Program.cs
@@ -7,8 +7,17 @@ namespace NoteBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program ");
-            CreateContacts name=new CreateContacts();
-            name.PersonDetails();
+            //CreateContacts name = new CreateContacts();
+            //name.PersonDetails();
+
+            AddNewContact obj = new AddNewContact();
+             obj.AddNewContactInBook();
+
+            //EditDetails obj = new EditDetails();
+            //obj.EditDetailsPerson();
+
+            //EditAddressBook ed=new EditAddressBook();
+            //ed.EditAddressBookJ();
         }
     }
 }
