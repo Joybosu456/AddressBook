03aefe188849c1be79e9207cd1f19704cb69301c
JOY <joybosu@gmail.com>
2022-02-27 01:03:33 +0530

    upload UC3 program file

---
+++ AddNewContact.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    public class AddNewContact
+    {
+        public void AddNewContactInBook()
+        {
+            {
+               
+                
+                Console.WriteLine("Press 1 to Add an entry to the address book :");
+                
+                Console.ReadLine();
+
+                Console.WriteLine("Please Enter your First Name=:");
+                string FIrst_Name = "FirstName";
+                FIrst_Name = Console.ReadLine();
+                string valueString1 = FIrst_Name;
+
+                Console.WriteLine("Please Enter your last Name=:");
+                string Last_Name = "LastName";
+                Last_Name = Console.ReadLine();
+                string valueString2 = Last_Name;
+
+                Console.WriteLine("Please Enter your Address, house number and town");
+                string Add = "Address";
+                Add = Console.ReadLine();
+                string valueString3 = Add;
+
+                Console.WriteLine("Please Enter your Country");
+                string coun = "Country";
+                coun = Console.ReadLine();
+                string valueString4 = coun;
+
+                Console.WriteLine("Please Enter your State:");
+                string stat = "State";
+                stat = Console.ReadLine();
+                string valueString5 = stat;
+
+                Console.WriteLine("Please Enter your Postcode");
+                string code = "Postcode";
+                code = Console.ReadLine();
+                string valueString6 = code;
+
+                Console.WriteLine("Please Enter your Email:");
+                string Email = "Email";
+                Email = Console.ReadLine();
+                string valueString7 = Email;
+
+                Console.WriteLine("Please Enter your Phone number:");
+                string Num = "PhoneNum";
+                Num = Console.ReadLine();
+                string valueString8 = Num;
+
+                Console.WriteLine("Your Address Book is Processing please enter the number What you want");
+                Console.WriteLine("---***---***---***---***---***---***---***---***---***---***---***---");
+                Console.WriteLine("Enter a number to Continue.........");
+                Console.ReadLine();
+
+                Console.WriteLine("Lastname you entered: " + valueString1);
+                Console.WriteLine("Firstname you entered: " + valueString2);
+                Console.WriteLine("Address and house number and town you entered: " + valueString3);
+                Console.WriteLine(" Enter state name:" + valueString4);
+                Console.WriteLine("County you entered: " + valueString5);
+                Console.WriteLine("Postcode you entered: " + valueString6);
+                Console.WriteLine("Enter your Mail Adress: " + valueString7);
+                Console.WriteLine("Enter your phone number: " + valueString8);
+                Console.ReadLine();
+
+            }
+        }
+
+        
+        
+    }
+}
